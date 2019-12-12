ea37ef391394881c5101d2d9b5a16c43d102ea1b
MateuszPaslawski <52893118+MateuszPaslawski@users.noreply.github.com>
2019-12-12 14:50:08 +0100

    InitialCommit

---
+++ SekwencjomatTranscoder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SekwencjomatTranscoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
