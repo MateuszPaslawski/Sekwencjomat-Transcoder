c667c28a3bd186d7f9d42520161b30ee72613b18
MateuszPaslawski <52893118+MateuszPaslawski@users.noreply.github.com>
2019-12-12 15:33:13 +0100

    First Testin Commit

--- SekwencjomatTranscoder/Program.cs
+++ SekwencjomatTranscoder/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using IniParser;
+using IniParser.Model;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +11,33 @@ namespace SekwencjomatTranscoder
 {
     class Program
     {
+        private static string FFmpegPath = string.Empty;
+        private static string INIfilePath = string.Empty;
+        
         static void Main(string[] args)
         {
+            if (args.Count() > 1)
+            {
+                Console.WriteLine($"Podano niewłaściwą ilośc argumentów: [{args.Count()}]\nMaksymalna wartość to: [1]");
+                return;
+            }
+
+            if (args.Count() == 1)
+            {
+
+                if (!File.Exists(args.First()))
+                {
+                    Console.WriteLine($"Podany plik nie istanieje: [{args.First()}]");
+                    return;
+                }
+                INIfilePath = args.First();
+            }
+
+            var parser = new FileIniDataParser();
+            IniData data = parser.ReadFile(INIfilePath);
+
+            Console.WriteLine(data["Test"]["testvalue"]); 
+            Console.ReadKey();
         }
     }
 }
